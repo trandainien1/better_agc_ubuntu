fbb10a37cb1f18084771716a522c1f7cc83c3502
trandainien1 <trandainien1@gmail.com>
2025-01-09 08:48:11 +0700

    fix img num

--- Methods/Better_AGCAM/Better_AGCAM.py
+++ Methods/Better_AGCAM/Better_AGCAM.py
@@ -1,8 +1,8 @@
 import torch
 from einops.layers.torch import Reduce, Rearrange
 
-class Better_AGCAM:
-    """ Implementation of our method."""
+
+class BetterAGC:
     def __init__(self, model, attention_matrix_layer = 'before_softmax', attention_grad_layer = 'after_softmax', head_fusion='sum', layer_fusion='sum'):
         """
         Args:
@@ -25,19 +25,18 @@ class Better_AGCAM:
                 module.register_forward_hook(self.get_attn_matrix)
             if attention_grad_layer in name:
                 module.register_full_backward_hook(self.get_grad_attn)
-                
+
     def get_attn_matrix(self, module, input, output):
         # As stated in Methodology part, in ViT with [class] token, only the first row of the attention matrix is directly connected with the MLP head.
-        self.attn_matrix.append(output[:, :, 0:1, :]) # shape: [batch, num_heads, 1, num_patches] 
-        
+        self.attn_matrix.append(output[:, :, 0:1, :]) # shape: [batch, num_heads, 1, num_patches]
 
 
     def get_grad_attn(self, module, grad_input, grad_output):
         # As stated in Methodology part, in ViT with [class] token, only the first row of the attention matrix is directly connected with the MLP head.
-        self.grad_attn.append(grad_output[0][:, :, 0:1, :]) # shape: [batch, num_heads, 1, num_patches] 
-        
-    
-    def generate(self, input_tensor, cls_idx=None):
+        self.grad_attn.append(grad_output[0][:, :, 0:1, :]) # shape: [batch, num_heads, 1, num_patches]
+
+
+    def generate_cams_of_heads(self, input_tensor, cls_idx=None):
         self.attn_matrix = []
         self.grad_attn = []
 
@@ -45,7 +44,7 @@ class Better_AGCAM:
         self.model.zero_grad()
         output = self.model(input_tensor)
         _, prediction = torch.max(output, 1)
-        self.prediction = prediction  
+        self.prediction = prediction
         if cls_idx==None:                               # generate CAM for a certain class label
             loss = output[0, prediction[0]]
         else:                                           # generate CAM for the predicted class
@@ -76,23 +75,90 @@ class Better_AGCAM:
         attn = torch.sigmoid(attn) # Here, the variable attn is the attention score matrices newly normalized with sigmoid, which are eqaul to the feature maps F^k_h in Equation 2 in the methodology part.
         mask = gradient * attn
 
-        # print(f'Masks result: {mask.shape}')
-
         # aggregation of CAM of all heads and all layers and reshape the final CAM.
         mask = mask[:, :, :, 1:].unsqueeze(0) # * niên: chỗ này thêm 1 ở đầu (ví dụ: (2) -> (1, 2)) và 1: là bỏ token class
         # print(mask.shape)
 
-        # ------------- original -----------------------
-        # mask = Reduce('b l h z p -> b l z p', reduction=self.head_fusion)(mask)
-        # print(mask.shape)
-        # mask = Reduce('b l z p -> b z p', reduction=self.layer_fusion)(mask)
-        # print(mask.shape)
-        # mask = Rerrange('b z (h w) -> b z h w', h=self.width, w=self.width)(mask)
-        # print(mask.shape)
-        
         # *Niên:Thay vì tính tổng theo blocks và theo head như công thức để ra 1 mask cuối cùng là CAM thì niên sẽ giữ lại tất cả các mask của các head ở mỗi block
         mask = Rearrange('b l hd z (h w)  -> b l hd z h w', h=self.width, w=self.width)(mask) # *Niên: chỗ này tách từng token (1, 196) thành từng patch (1, 14, 14)
-        
-        # print(mask.shape)
+
         return prediction, mask, output
 
+    def generate_scores(self, head_cams, prediction, output_truth, image):
+        with torch.no_grad():
+            tensor_heatmaps = head_cams[0]
+            tensor_heatmaps = tensor_heatmaps.reshape(144, 1, 14, 14)
+            tensor_heatmaps = transforms.Resize((224, 224))(tensor_heatmaps)
+    
+            # Compute min and max along each image
+            min_vals = tensor_heatmaps.amin(dim=(2, 3), keepdim=True)  # Min across width and height
+            max_vals = tensor_heatmaps.amax(dim=(2, 3), keepdim=True)  # Max across width and height
+            # Normalize using min-max scaling
+            tensor_heatmaps = (tensor_heatmaps - min_vals) / (max_vals - min_vals + 1e-7)  # Add small value to avoid division by zero
+            # print("before multiply img with mask: ")
+            # print(torch.cuda.memory_allocated()/1024**2)
+            m = torch.mul(tensor_heatmaps, image)
+            # print("After multiply img with mask scores: ")
+            # print(torch.cuda.memory_allocated()/1024**2)
+
+            with torch.no_grad():
+                output_mask = self.model(m)
+            
+            # print("After get output from model: ")
+            # print(torch.cuda.memory_allocated()/1024**2)
+    
+            agc_scores = output_mask[:, prediction.item()] - output_truth[0, prediction.item()]
+            agc_scores = torch.sigmoid(agc_scores)
+    
+            agc_scores = agc_scores.reshape(head_cams[0].shape[0], head_cams[0].shape[1])
+
+            del output_mask  # Delete unnecessary variables that are no longer needed
+            torch.cuda.empty_cache()  # Clean up cache if necessary
+            # print("After deleted output from model: ")
+            # print(torch.cuda.memory_allocated()/1024**2)
+            
+            return agc_scores
+
+    def generate_saliency(self, head_cams, agc_scores):
+        mask = (agc_scores.view(12, 12, 1, 1, 1) * head_cams[0]).sum(axis=(0, 1))
+
+        mask = mask.squeeze()
+        return mask
+
+    def __call__(self, x, class_idx=None):
+
+        # Check that we get only one image
+        assert x.dim() == 3 or (x.dim() == 4 and x.shape[0] == 1), "Only one image can be processed at a time"
+
+        # Unsqueeze to get 4 dimensions if needed
+        if x.dim() == 3:
+            x = x.unsqueeze(dim=0)
+
+        with torch.enable_grad():
+            predicted_class, head_cams, output_truth = self.generate_cams_of_heads(x)
+
+        # print("After generate cams: ")
+        # print(torch.cuda.memory_allocated()/1024**2)
+        # print()
+        
+        # Define the class to explain. If not explicit, use the class predicted by the model
+        if class_idx is None:
+            class_idx = predicted_class
+            print("class idx", class_idx)
+
+        # Generate the saliency map for image x and class_idx
+        scores = self.generate_scores(
+            image=x,
+            head_cams=head_cams,
+            prediction=predicted_class, output_truth=output_truth
+        )
+        # print("After generate scores: ")
+        # print(torch.cuda.memory_allocated()/1024**2)
+        # print()
+        
+        saliency_map = self.generate_saliency(head_cams=head_cams, agc_scores=scores)
+        # print("After generate saliency maps: ")
+        # print(torch.cuda.memory_allocated()/1024**2)
+        # print()
+
+        return predicted_class, saliency_map
