39deaa7c9c8b734ee6331b79b62c2e78e19482e1
trandainien1@gmail.com <trandainien1@gmail.com>
2025-01-26 19:59:32 +0700

    fix error

--- Methods/Better_AGCAM/Better_AGCAM.py
+++ Methods/Better_AGCAM/Better_AGCAM.py
@@ -1043,8 +1043,8 @@ class BetterAGC_cluster_add_noise:
             H = tensor_heatmaps.shape[2]
             W = tensor_heatmaps.shape[3]
             # Generate the inverse of masks, i.e., 1-M_i
-            # masks_inverse=torch.from_numpy(np.repeat((1-tensor_heatmaps.cpu().numpy())[:, :, np.newaxis,:], 3, axis=2)).cuda()
-            masks_inverse= 1 - tensor_heatmaps
+            masks_inverse=torch.from_numpy(np.repeat((1-tensor_heatmaps.cpu().numpy())[:, :, np.newaxis,:], 3, axis=2)).cuda()
+            masks_inverse = masks_inverse.squeeze(1)
 
             print('[DEBUG INVERSE MASK]', masks_inverse.shape)
             # Generate the random Gaussian noise
