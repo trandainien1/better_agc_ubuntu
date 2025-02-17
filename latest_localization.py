1120c5f7256cdc0b1001c0ce9fa1188d8badc555
trandainien1 <trandainien1@gmail.com>
2025-02-17 17:01:06 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -215,7 +215,7 @@ with torch.enable_grad():
         else:
             prediction, saliency_map = method.generate(image) # [1, 1, 14, 14]
 
-        print('[DEBUG]', prediction==label)
+        # print('[DEBUG]', prediction==label)
         if prediction!=label:
             continue
         # If the model produces the wrong predication, the heatmap is unreliable and therefore is excluded from the evaluation.
