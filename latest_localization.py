0439b4ee030b9db50e78babef02d7ff3c94672b8
trandainien1 <trandainien1@gmail.com>
2025-02-17 19:17:08 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -34,6 +34,7 @@ from Methods.LRP.ViT_explanation_generator import LRP
 
 from Methods.AttentionRollout.AttentionRollout import VITAttentionRollout
 from Methods.TIS.tis import TISWrapper
+from Methods.BT.bt import BTTWrapper, BTHWrapper
 
 import csv
 from csv import DictWriter
@@ -188,6 +189,18 @@ elif METHOD == 'tis':
     model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
     model = model.eval()
     method = TISWrapper(model=model)
+elif METHOD == 'vitcx':
+    model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
+    model = model.eval()
+    method = TISWrapper(model=model)
+elif METHOD == 'btt':
+    model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
+    model = model.eval()
+    method = BTTWrapper(model=model)
+elif METHOD == 'bth':
+    model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
+    model = model.eval()
+    method = BTHWrapper(model=model)
     
 model = model.to('cuda')
 
