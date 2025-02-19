f5072afc00ac0d4fb867914b305ceebcba7aa5c4
trandainien1 <trandainien1@gmail.com>
2025-02-19 14:49:17 +0700

    update

--- save_h5.py
+++ save_h5.py
@@ -64,11 +64,24 @@ data_root = args.data_root
 test_transform = transforms.Compose([
     transforms.Resize((224,224)),
     transforms.ToTensor(),
-    transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])
+    # transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
+# unnormalize = transforms.Compose([
+#     transforms.Normalize([0., 0., 0.], [1/0.5, 1/0.5, 1/0.5]),
+#     transforms.Normalize([-0.5, -0.5, -0.5], [1., 1., 1.,])
+# ])
+
+# Define the normalization parameters
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+# Create the unnormalize transform
 unnormalize = transforms.Compose([
-    transforms.Normalize([0., 0., 0.], [1/0.5, 1/0.5, 1/0.5]),
-    transforms.Normalize([-0.5, -0.5, -0.5], [1., 1., 1.,])
+    transforms.Normalize(
+        mean=[-m/s for m, s in zip(mean, std)],  # Reverse mean: -mean/std
+        std=[1/s for s in std]                   # Reverse std: 1/std
+    )
 ])
 
 validset = ImageNetDataset_val(
