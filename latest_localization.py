827717bf4d7c9a743ec40214d5fdcf0704984249
trandainien1 <trandainien1@gmail.com>
2025-02-19 18:51:53 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -123,7 +123,7 @@ if METHOD == 'better_agc_plus1':
     model = ViT_Ours.create_model(MODEL, pretrained=True, num_classes=class_num).to('cuda')
     model.load_state_dict(state_dict, strict=True)
     model.eval()
-    method = BetterAGC_plus1(model, score_minmax_norm=True)
+    method = BetterAGC_plus1(model, score_minmax_norm=True, normalize_cam_heads=False)
 elif METHOD == 'better_agc':
     state_dict = model_zoo.load_url('https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_base_p16_224-80ecf9dd.pth', progress=True, map_location='cuda')
     model = ViT_Ours.create_model(MODEL, pretrained=True, num_classes=class_num).to('cuda')
