8b8182a4f68af73c4d0d114d4e36f99cdaeed58b
trandainien1 <trandainien1@gmail.com>
2025-02-19 20:13:42 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -211,7 +211,7 @@ elif METHOD == 'bth':
     
 model = model.to('cuda')
 
-print(f"[XAI METHOD]: {METHOD} - Head fusion: mean")
+print(f"[XAI METHOD]: {METHOD} - Head fusion: sum - add noise")
 
 validloader = DataLoader(
     dataset = validset,
