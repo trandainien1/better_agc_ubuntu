e8ee5634e9644595c1cde19f84bd5db6ee789aee
trandainien1 <trandainien1@gmail.com>
2025-02-22 10:42:49 +0700

    update

--- save_h5.py
+++ save_h5.py
@@ -167,6 +167,9 @@ with torch.enable_grad():
         # generate heatmap
         prediction, heatmap = method.generate(image, label)
 
+         if args.method == 'tis':
+            heatmap = heatmap.reshape(1, 1, 14, 14) 
+        
         # resize the heatmap
         print('[DEBUG]', heatmap.shape)
         resize = transforms.Resize((224, 224))
