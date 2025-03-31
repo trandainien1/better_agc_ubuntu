72b49f2c111f2f312d4a5d44c8ac7a6f7cd8b539
trandainien1 <trandainien1@gmail.com>
2025-03-31 16:49:35 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -309,7 +309,6 @@ with torch.enable_grad():
 
         # print('[DEBUG] PREDICTION', prediction)
         # print('[DEBUG] LABEL', label.item())
-        print('[DEBUG]: ', prediction, label.item())
         if prediction!=label.item():
             continue
         # If the model produces the wrong predication, the heatmap is unreliable and therefore is excluded from the evaluation.
