ce605cf3e25199f50b74b33b9d945db1a9949c0d
trandainien1 <trandainien1@gmail.com>
2025-04-01 12:23:49 +0700

    update

--- Datasets/ILSVRC.py
+++ Datasets/ILSVRC.py
@@ -174,6 +174,7 @@ class Cub2011(Dataset):
         }
         return sample_dict
 
+# ------------- from github CUB SAM ----------------
 # def read_text(file_path):
 #     with open(file_path, 'r') as f:
 #         content = f.read()
@@ -184,51 +185,51 @@ class Cub2011(Dataset):
 #     return cv2.imread(file_path)[...,[2,1,0]]
 
 # class Cub2011:
-    def __init__(self, root_path='data/CUB_200_2011') -> None:
-        super().__init__()
-        self.image_paths = read_text(pth.join(root_path, 'images.txt'))
-        self.bboxes = read_text(pth.join(root_path, 'bounding_boxes.txt')) # box means background-foreground binary segmentation
-        self.parts = read_text(pth.join(root_path, 'parts', 'part_locs.txt')) # fine-grained segmentation for different parts
-        self.clicks = read_text(pth.join(root_path, 'parts', 'part_click_locs.txt')) # more points for each part
-        self.root_path = root_path
+    # def __init__(self, root_path='data/CUB_200_2011') -> None:
+    #     super().__init__()
+    #     self.image_paths = read_text(pth.join(root_path, 'images.txt'))
+    #     self.bboxes = read_text(pth.join(root_path, 'bounding_boxes.txt')) # box means background-foreground binary segmentation
+    #     self.parts = read_text(pth.join(root_path, 'parts', 'part_locs.txt')) # fine-grained segmentation for different parts
+    #     self.clicks = read_text(pth.join(root_path, 'parts', 'part_click_locs.txt')) # more points for each part
+    #     self.root_path = root_path
         
-    def __call__(self, index: Any) -> Any:
-        image_path_line = self.image_paths[index]
-        index, image_path = image_path_line.split(' ')
-        full_image_path = pth.join(self.root_path, 'images', image_path)
-        image = read_image(full_image_path)
+    # def __call__(self, index: Any) -> Any:
+    #     image_path_line = self.image_paths[index]
+    #     index, image_path = image_path_line.split(' ')
+    #     full_image_path = pth.join(self.root_path, 'images', image_path)
+    #     image = read_image(full_image_path)
 
-        bboxes = list(filter(lambda x: x.split(' ')[0] == index, self.bboxes))
-        parts = list(filter(lambda x: x.split(' ')[0] == index, self.parts))
+    #     bboxes = list(filter(lambda x: x.split(' ')[0] == index, self.bboxes))
+    #     parts = list(filter(lambda x: x.split(' ')[0] == index, self.parts))
         
-        bboxes = list(map(lambda x: np.array(x.split(' ')[1:], dtype=np.float64), bboxes))
-        bboxes = list(map(lambda x: [x[0], x[1], x[0] + x[2], x[1] + x[3]], bboxes))
+    #     bboxes = list(map(lambda x: np.array(x.split(' ')[1:], dtype=np.float64), bboxes))
+    #     bboxes = list(map(lambda x: [x[0], x[1], x[0] + x[2], x[1] + x[3]], bboxes))
         
-        parts = list(map(lambda x: np.array(x.split(' ')[1:], np.float64), parts)) # (type, x, y, status)
-        parts = list(filter(lambda x: x[-1], parts)) # only keep valid annotations
+    #     parts = list(map(lambda x: np.array(x.split(' ')[1:], np.float64), parts)) # (type, x, y, status)
+    #     parts = list(filter(lambda x: x[-1], parts)) # only keep valid annotations
         
-        point_coords = list(map(lambda x: (x[1], x[2]), parts))
-        point_labels = list(map(lambda x: int(x[0]), parts))
+    #     point_coords = list(map(lambda x: (x[1], x[2]), parts))
+    #     point_labels = list(map(lambda x: int(x[0]), parts))
         
-        clicks = list(filter(lambda x: x.split(' ')[0] == index, self.clicks))
-        clicks = list(map(lambda x: np.array(x.split(' ')[1:], np.float64), clicks))
-        clicks = list(filter(lambda x: x[-2], clicks)) # only keep valid annotations
-        click_coords = list(map(lambda x: (x[1], x[2]), clicks))
-        click_labels = list(map(lambda x: int(x[0]), clicks))        
+    #     clicks = list(filter(lambda x: x.split(' ')[0] == index, self.clicks))
+    #     clicks = list(map(lambda x: np.array(x.split(' ')[1:], np.float64), clicks))
+    #     clicks = list(filter(lambda x: x[-2], clicks)) # only keep valid annotations
+    #     click_coords = list(map(lambda x: (x[1], x[2]), clicks))
+    #     click_labels = list(map(lambda x: int(x[0]), clicks))        
         
-        # return image_path, image, bboxes, point_coords, point_labels, click_coords, click_labels
-        sample = {
-            'image': image, 
-            'label': index, 
-            'filename': image_path, 
-            'num_objects': 1, 
-            'bnd_box': bboxes, 
-            'img_path': image_path
-        }
-        return sample
+    #     # return image_path, image, bboxes, point_coords, point_labels, click_coords, click_labels
+    #     sample = {
+    #         'image': image, 
+    #         'label': index, 
+    #         'filename': image_path, 
+    #         'num_objects': 1, 
+    #         'bnd_box': bboxes, 
+    #         'img_path': image_path
+    #     }
+    #     return sample
     
-    def __len__(self):
-        return len(self.image_paths)
+    # def __len__(self):
+    #     return len(self.image_paths)
     
-    def __repr__(self) -> str:
-        pass
+    # def __repr__(self) -> str:
+    #     pass
