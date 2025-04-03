fb9d79d3a9ad662453238b235000314e762bd946
trandainien1 <trandainien1@gmail.com>
2025-04-03 12:30:15 +0700

    update

--- localization.py
+++ localization.py
@@ -126,7 +126,7 @@ with torch.enable_grad():
         image = data['image'].to(device)
         label = data['label'].to(device)
         bnd_box = data['bnd_box'].to(device).squeeze(0)
-        
+        print(bnd_box)
         prediction, mask = method.generate(image)
         predictions.append(prediction)
 
