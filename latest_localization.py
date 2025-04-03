40e6543d2123018a8fe5b47eb5cb7c65ad61b392
trandainien1 <trandainien1@gmail.com>
2025-04-03 12:41:07 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -295,6 +295,7 @@ with torch.enable_grad():
 
         # bnd_box = data['bnd_box'].to('cuda').squeeze(0) # for Image Net
         obj = target[0]["annotation"]["object"][0]
+        print(obj)
         bbox = obj["bndbox"]
         xmin = int(bbox["xmin"])
         ymin = int(bbox["ymin"])
