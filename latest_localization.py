03afbf26c085f58210480d8891d2ec0cdbea3a5e
trandainien1 <trandainien1@gmail.com>
2025-04-13 10:13:30 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -227,7 +227,8 @@ elif METHOD == 'better_agc_cluster_add_noise':
 elif METHOD == 'chefer1':
     state_dict = torch.load('/kaggle/working/better_agc_ubuntu/vit_pascal_voc_60.pth', weights_only=True)
     # state_dict = model_zoo.load_url('https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_base_p16_224-80ecf9dd.pth', progress=True, map_location='cuda')
-    model = LRP_vit_base_patch16_224('cuda', num_classes=1000).to('cuda')
+    model = LRP_vit_base_patch16_224('cuda', num_classes=20).to('cuda')
+    model.head = nn.Linear(model.head.in_features, 20).to('cuda'
     model.load_state_dict(state_dict['model_state'], strict=True)
     model.eval()
     method = LRP(model, device='cuda')
