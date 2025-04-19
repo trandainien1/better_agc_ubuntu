56930a3e0b9273509251d343b5a13320f9e0b550
trandainien1 <trandainien1@gmail.com>
2025-04-19 13:32:36 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -250,7 +250,8 @@ elif METHOD == 'vitcx':
     method = ViTCXWrapper(model=model)
 elif METHOD == 'btt':
     model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
-
+    state_dict = torch.load('/kaggle/working/better_agc_ubuntu/vit_pascal_voc_60.pth', weights_only=True)
+    model.load_state_dict(state_dict['model_state']
     model = model.eval()
     method = BTTWrapper(model=model)
 elif METHOD == 'bth':
