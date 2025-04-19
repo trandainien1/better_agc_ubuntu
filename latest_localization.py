3a9da807e4606cd98c86e0bfb725539ce80f3902
trandainien1 <trandainien1@gmail.com>
2025-04-20 03:47:50 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -239,7 +239,10 @@ elif METHOD == 'tam':
     model = model.eval()
     method = TAMWrapper(model=model)
 elif METHOD == 'tis':
-    model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
+    model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k', num_classes=20)
+    model.head = nn.Linear(model.head.in_features, 20)
+    state_dict = torch.load('/kaggle/working/better_agc_ubuntu/vit_pascal_voc_60.pth', weights_only=False)
+    model.load_state_dict(state_dict['model_state'])
     model = model.eval()
     method = TISWrapper(model=model)
 elif METHOD == 'vitcx':
