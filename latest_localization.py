3b8069681e6685faddf3b2b8fe5e3a3b7d26379a
trandainien1 <trandainien1@gmail.com>
2025-04-19 13:33:01 +0700

    update

--- latest_localization.py
+++ latest_localization.py
@@ -231,7 +231,7 @@ elif METHOD == 'rollout':
     model.eval()
     method = VITAttentionRollout(model, device=device)
 elif METHOD == 'chefer2':
-    model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
+    model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k', num_classes=20)
     state_dict = torch.load('/kaggle/working/better_agc_ubuntu/vit_pascal_voc_60.pth', weights_only=True)
     model.load_state_dict(state_dict['model_state'])
     model = model.eval()
