8a272c69aa1ace7ae8e8641e04d780ed1a172c45
trandainien1 <trandainien1@gmail.com>
2025-05-11 14:09:35 +0700

    fix

--- latest_localization.py
+++ latest_localization.py
@@ -219,8 +219,8 @@ elif METHOD == 'agc':
         model.load_state_dict(state_dict['model_state'])
     model.eval()
     
-    method = AGCAM(model)
-    print('CUSTOM CONFIG: original')
+    method = AGCAM(model, head_fusion='mean')
+    print('CUSTOM CONFIG: head_fusion = mean')
 elif METHOD == 'better_agc_cluster':
     state_dict = model_zoo.load_url('https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_base_p16_224-80ecf9dd.pth', progress=True, map_location='cuda')
     model = ViT_Ours.create_model(MODEL, pretrained=True, num_classes=class_num).to('cuda')
