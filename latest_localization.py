48f46687b9f84f879139a99398595e46819bb73a
trandainien1 <trandainien1@gmail.com>
2025-05-11 14:00:40 +0700

    fix

--- latest_localization.py
+++ latest_localization.py
@@ -343,8 +343,8 @@ with torch.enable_grad():
 
             # To avoid the overlapping problem of the bounding box labels, we generate a 0-1 segmentation mask from the bounding box label.
 
-            seg_label = box_to_seg(bnd_box.unsqueeze(0)).to('cuda') # PASCAL VOC
-            # seg_label = box_to_seg(bnd_box).to('cuda') # Imagenet 
+            # seg_label = box_to_seg(bnd_box.unsqueeze(0)).to('cuda') # PASCAL VOC
+            seg_label = box_to_seg(bnd_box).to('cuda') # Imagenet 
 
 
             # From the generated heatmap, we generate a bounding box and then convert it to a segmentation mask to compare with the bounding box label.
